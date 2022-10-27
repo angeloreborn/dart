9e22676c460226b728320608a660b9b4cbf6692a
angeloreborn <angelowolffreborn@gmail.com>
2022-10-28 00:51:00 +0200

    implement diagnostic base model properties

--- dart-core-api/Schemas/DiagnosticSchema/DiagnosticModel.cs
+++ dart-core-api/Schemas/DiagnosticSchema/DiagnosticModel.cs
@@ -1,4 +1,6 @@
 ﻿using dart_core_api.Models;
+using System.ComponentModel;
+using System.Diagnostics;
 using TypeGen.Core.TypeAnnotations;
 
 namespace dart_core_api.Schemas.DiagnosticSchema
@@ -7,6 +9,17 @@ namespace dart_core_api.Schemas.DiagnosticSchema
     [ExportTsInterface]
     public class DiagnosticModel : BaseModel
     {
-
+        [DefaultValue(-1)]
+        public double NaRuntime { get; set; }
+        [DefaultValue(-1)]
+        public double MsRuntime { get; set; }
+        [DefaultValue(-1)]
+        public double ScRuntime { get; set; }
+        public DiagnosticModel(long nanoRuntime) 
+        {
+            NaRuntime = nanoRuntime;
+            MsRuntime = nanoRuntime * 0.000001;
+            ScRuntime = nanoRuntime * 0.000000001;
+        }
     }
 }
