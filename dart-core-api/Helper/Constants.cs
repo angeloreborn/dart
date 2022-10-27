8603c7f4f8a510a0d4820d97457d1cc5d6ea308f
angeloreborn <angelowolffreborn@gmail.com>
2022-10-27 23:44:51 +0200

    create base database options model

---
+++ dart-core-api/Helper/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace dart_core_api.Helper
+{
+    public static class SemanticConstants
+    {
+        public const long ONE_MILLION = 1000000;
+    }
+}
--- dart-core-api/Schemas/ConfigSchema/DatabaseOptionsModel.cs
+++ dart-core-api/Schemas/ConfigSchema/DatabaseOptionsModel.cs
@@ -1,4 +1,6 @@
 ﻿using dart_core_api.Attributes.ReadonlyAnnotations;
+using dart_core_api.Helper;
+using System.ComponentModel;
 using TypeGen.Core.TypeAnnotations;
 
 namespace dart_core_api.Schemas.ConfigSchema
@@ -6,7 +8,13 @@ namespace dart_core_api.Schemas.ConfigSchema
     /// <summary>
     /// Should not generate <see cref="ExportTsClassAttribute"/> or <see cref="ExportTsInterfaceAttribute"/>
     /// </summary>
-    public class DatabaseOptionsModel
+    public class BaseDatabaseOptionsModel
     {
+        [DefaultValue(false)]
+        public bool TrackStateChanges { get; set; }
+        [DefaultValue(false)]
+        public bool TrackDetailedStateChanges { get; set; }
+        [DefaultValue(long.MaxValue)]
+        public long MaximumRecords { get; set; }
     }
 }
