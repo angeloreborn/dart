2aace9ac17f5e2650996500f13993bcdd3b3ad13
angeloreborn <angelowolffreborn@gmail.com>
2022-10-28 00:05:26 +0200

    implement static application configuration

---
+++ dart-core-api/Config/DatabaseOptionsModel.cs
@@ -0,0 +1,42 @@
+﻿using dart_core_api.Attributes.ReadonlyAnnotations;
+using dart_core_api.Helper;
+using System.ComponentModel;
+using TypeGen.Core.TypeAnnotations;
+
+namespace dart_core_api.Config
+{
+    /// <summary>
+    /// Should not generate <see cref="ExportTsClassAttribute"/> or <see cref="ExportTsInterfaceAttribute"/>
+    /// </summary> 
+
+    public static class Config
+    {
+        public static bool Initialize() => default;
+        public static class Database
+        {
+            /// <summary>
+            /// controls the state and property tracking of ef core operations
+            /// </summary>
+            public static class Tracking
+            {
+                /// <summary>
+                /// will track the state of the operation performed
+                /// </summary>
+                [DefaultValue(false)]
+                public static bool TrackStateChanges { get; set; }
+                /// <summary>
+                /// will track the property value changes
+                /// </summary>
+                [DefaultValue(false)]
+                public static bool TrackDetailedStateChanges { get; set; }
+            }
+
+            /// <summary>
+            /// sets maximum records for intance of the database context
+            /// </summary>
+            [DefaultValue(long.MaxValue)]
+            public static long MaximumRecords { get; set; }
+        }
+    }
+  
+}
