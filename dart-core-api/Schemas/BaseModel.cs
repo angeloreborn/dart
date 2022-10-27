421fb2677ac63244c14babc469dbf3b5b6e30e89
angeloreborn <angelowolffreborn@gmail.com>
2022-10-28 00:52:44 +0200

    decouple base model attributes to model component interface pattern

--- dart-core-api/Schemas/BaseModel.cs
+++ dart-core-api/Schemas/BaseModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace dart_core_api.Models
@@ -9,7 +10,15 @@ namespace dart_core_api.Models
         public long Id { get; set; }
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime CreatedDate { get; set; }
+    }
+    public interface IModifiable
+    {
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime ModifiedDate { get; set; }
+    }
+    public interface ISoftDeletable
+    {
+        [DefaultValue(false)]
+        public bool SoftDeleted { get; set; }
     }
 }
