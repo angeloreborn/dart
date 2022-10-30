80670eb0b88f75720bfa6d6f56889a433bcaadb6
angeloreborn <angelowolffreborn@gmail.com>
2022-10-31 00:38:07 +0200

    update windows os file path async search enumeration yield method

--- dart-core-api/Services/Machine/Darwin.cs
+++ dart-core-api/Services/Machine/Darwin.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using dart_core_api.Helper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Versioning;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +11,28 @@ namespace dart_core_api.Services.Machine
 {
     public interface IDarwin
     {
-
+        WindowsIdentity? GetIdentity();
+        IAsyncEnumerable<string> Search(string folder, string extention);
     }
+
+    [SupportedOSPlatform(OSPlatformType.Windows)]
     public class Darwin
     {
-        public OperatingSystem GetOperatingSystemInfo()
+        private WindowsIdentity? _identity;
+        public Darwin()
         {
-            return Environment.OSVersion;
+            _identity = GetIdentity();
         }
 
+        public WindowsIdentity? GetIdentity() => WindowsIdentity.GetCurrent();
 
+        public async IAsyncEnumerable<string> Search(string folder, string extention)
+        {
+            await Task.Run(() => { }); // or supress
+            foreach (string file in Directory.EnumerateFiles(folder, $"*{extention}", SearchOption.AllDirectories))
+            {
+                yield return file;
+            }
+        }
     }
 }
