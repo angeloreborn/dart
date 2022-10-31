7c0a77a4bb3ceb443b91ba7a09260928268329d3
angeloreborn <angelowolffreborn@gmail.com>
2022-10-31 22:13:16 +0200

    implement dynamic parameter binding on service handler for service factory, sgnlr params and hub

--- dart-core-api/Hubs/ContainerHub.cs
+++ dart-core-api/Hubs/ContainerHub.cs
@@ -1,15 +1,15 @@
 ﻿using dart_core_api.Services.Diagnostic;
 using dart_core_api.Services.System;
-using dart_schema.Diagnostic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace dart_core_api.Hubs
 {
     public class ContainerHub : Hub
     {
-        public async Task ServiceHandler(string container,  string method, object[] obj, 
+        public async Task ServiceHandler(string container, string method, object[] obj,
             [FromServices] IServiceFactory serviceFactory)
         {
             DiagnosticTracker diagnosticTracker = new DiagnosticTracker();
@@ -19,10 +19,42 @@ namespace dart_core_api.Hubs
             if (serviceType == null) return;
             MethodInfo? serviceMethodInfo = serviceType.GetMethod(method);
             if (serviceMethodInfo == null) return;
-            var arg = serviceMethodInfo.Invoke(service, obj);
+            ParameterInfo[] paramInfo = serviceMethodInfo.GetParameters();
+            List<object> invokedParams = new List<object>();
+            foreach (var param in paramInfo)
+            {
+                object? paramValue = null;
+                foreach (object obj2 in obj)
+                {
+                    if (obj2.GetType() == param.ParameterType)
+                    {
+                        paramValue = obj2;
+                    }
+                }
+                if (paramValue == null)
+                {
+                    if (param.ParameterType == typeof(ContainerHub))
+                    {
+                        paramValue = this;
+                    }
+                    else
+                    {
+                        var serviceBinding = serviceFactory.GetServiceByType(param.ParameterType);
+                        if (serviceBinding != null)
+                        {
+                            paramValue = serviceBinding;
+                        }
+                        else
+                        {
+                            paramValue = Activator.CreateInstance(param.ParameterType);
+                        }
+                    }
+                }
+                if (paramValue == null) throw new NullReferenceException();
+                invokedParams.Add(paramValue);
+            }
+            var arg = serviceMethodInfo.Invoke(service, invokedParams.ToArray());
             await Clients.All.SendAsync(method, arg);
-            ServiceDiagnostic serviceDiagnositc = diagnosticTracker.Stop();
-
         }
     }
 }
