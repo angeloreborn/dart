0679c79626eb334d35f9c31c251e3c06629a414b
angeloreborn <angelowolffreborn@gmail.com>
2022-11-15 17:46:39 +0200

    add dart-lang

---
+++ dart-lang/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dart_lang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ dart-lang/Tokens/SeperatorTokens.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dart_lang.Tokens
+{
+    internal class SeperatorTokens
+    {
+    }
+}
